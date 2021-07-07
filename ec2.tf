cda3fd5f0eb27141f56ae613a9decce2c58d2073
kiran godi <78716315+kiragodi@users.noreply.github.com>
2021-07-07 11:50:24 +0530

    Create ec2.tf
    save changes

--- ec2.tf
+++ ec2.tf
@@ -8,12 +8,6 @@ terraform {
   }
 }
 
-provider "aws"{
-  region = "ap-south-1"
-  access_key = "AKIAWHNRJ7UIDRRSXY4A"
-  secret_key = "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"
-}
-
 resource "aws_instance" "ec2"{
   ami = "ami-011c99152163a87ae"
   instance_type = "t2.micro"
